f17548fe91b837c81118907f4946633c5f2ba0ed
Omega Network Founder <94934670+GamingReborn@users.noreply.github.com>
2022-11-16 15:02:41 +0000

    THEME CHANGER FINALLY DONEEEE i did this in history btw

--- static/js/theme.js
+++ static/js/theme.js
@@ -6,18 +6,19 @@ function midnight() {
 	window.localStorage.removeItem('classic');
 }
 //function midnight() {
-	//window.localStorage.setItem('midnight', 'on')
-	
-	//window.localStorage.setItem('classic', 'off')
-	//window.localStorage.removeItem('classic')
+//window.localStorage.setItem('midnight', 'on')
+
+//window.localStorage.setItem('classic', 'off')
+//window.localStorage.removeItem('classic')
 //}
 function classic() {
 	window.localStorage.setItem('classic', 'on')
+	document.body.style.backgroundColor = "#0E48A7"
 	window.localStorage.setItem('midnight', 'off')
 	window.localStorage.removeItem('midnight')
 }
 if (window.localStorage.getItem('classic') == "on") {
 	document.body.classList.add("classic")
-} else if (window.localStorage.getItem('midnight2') == "true") {
+} else if (window.localStorage.getItem('midnight') == "true") {
 	document.body.classList.add("midnight")
 }
