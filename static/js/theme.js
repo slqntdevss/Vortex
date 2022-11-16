c3dfc7f3120aabc6bf032d7844d459dc90d7012b
Omega Network Founder <94934670+GamingReborn@users.noreply.github.com>
2022-11-16 16:20:04 +0000

    added another theme + fixed a theme bug

--- static/js/theme.js
+++ static/js/theme.js
@@ -4,21 +4,29 @@ function midnight() {
 	document.body.style.backgroundColor = "#26324b"
 	localStorage.setItem('classic', 'false');
 	window.localStorage.removeItem('classic');
+	window.localStorage.setItem('lava', 'off')
+	window.localStorage.removeItem('lava')
 }
-//function midnight() {
-//window.localStorage.setItem('midnight', 'on')
-
-//window.localStorage.setItem('classic', 'off')
-//window.localStorage.removeItem('classic')
-//}
+function lava() {
+	window.localStorage.setItem('lava', 'on')
+	document.body.style.backgroundColor = "#fb4509"
+	window.localStorage.setItem('classic', 'off')
+	window.localStorage.removeItem('classic')
+	window.localStorage.setItem('midnight', 'off')
+	window.localStorage.removeItem('midnight')
+}
 function classic() {
 	window.localStorage.setItem('classic', 'on')
 	document.body.style.backgroundColor = "#0E48A7"
 	window.localStorage.setItem('midnight', 'off')
 	window.localStorage.removeItem('midnight')
+	window.localStorage.setItem('lava', 'off')
+	window.localStorage.removeItem('lava')
 }
 if (window.localStorage.getItem('classic') == "on") {
 	document.body.classList.add("classic")
-} else if (window.localStorage.getItem('midnight') == "true") {
+} else if (window.localStorage.getItem('midnight') == "on") {
 	document.body.classList.add("midnight")
+} else if (window.localStorage.getItem('lava') == "on") {
+	document.body.classList.add("lava")
 }
